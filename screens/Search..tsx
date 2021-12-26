c99bc803f74bbd2e13589c5e5f2c386a44568684
kimja01 <kimja7045@naver.com>
2021-12-26 16:22:22 +0900

    Add ts setup with allFiles

--- screens/Search..tsx
+++ screens/Search..tsx
@@ -1,7 +1,10 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Search = ({ navigation }) => {
+const Search: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({
+  navigation,
+}) => {
   return (
     <View>
       <Text>search</Text>
--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,7 +1,10 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Tv = ({ navigation }) => {
+const Tv: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({
+  navigation,
+}) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>tv</Text>
